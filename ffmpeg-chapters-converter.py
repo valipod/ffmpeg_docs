7001f4c60761a4fa66c506995dfc0805822d97e7
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2023-02-23 16:40:13 +0200

    add info about removing existing disposition update chapter conversion script

--- ffmpeg-chapters-converter.py
+++ ffmpeg-chapters-converter.py
@@ -4,12 +4,12 @@ chapters = list()
 
 with open('chapters.txt', 'r') as f:
     for line in f:
-        x = re.match(r"(\d{2}):(\d{2}):(\d{2})\.(\d{3}) (.*)", line)
-        hrs = int(x.group(1))
-        mins = int(x.group(2))
-        secs = int(x.group(3))
-        milisecs = int(x.group(4))
-        title = x.group(5)
+        x = re.match(r"(\S{7}\d{2})=(\d{2}):(\d{2}):(\d{2})\.(\d{3})", line)
+        title = x.group(1)
+        hrs = int(x.group(2))
+        mins = int(x.group(3))
+        secs = int(x.group(4))
+        milisecs = int(x.group(5))
 
         minutes = (hrs * 60) + mins
         seconds = secs + (minutes * 60)
@@ -22,18 +22,26 @@ with open('chapters.txt', 'r') as f:
 
 text = ""
 
-for i in range(len(chapters) - 1):
-    chap = chapters[i]
-    title = chap['title']
-    start = chap['startTime']
-    end = chapters[i + 1]['startTime'] - 1
-    text += f"""
+template = """
 [CHAPTER]
 TIMEBASE=1/1000
 START={start}
 END={end}
 title={title}
 """
+
+for i in range(len(chapters) - 1):
+    chap = chapters[i]
+    title = chap['title']
+    start = chap['startTime']
+    end = chapters[i + 1]['startTime'] - 1
+    text += """
+[CHAPTER]
+TIMEBASE=1/1000
+START={start}
+END={end}
+title={title}
+""".format(start=start, end=end, title=title)
 
 
 with open("chapters.ffmpeg.txt", "a") as myfile:
